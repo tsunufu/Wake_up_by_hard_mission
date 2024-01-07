6bbeb66cf555738e3cb4b695ae6208e67f828d2b
kyota <pitamilk1215@icloud.com>
2024-01-07 12:35:10 +0900

    8-カメラ機能

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'widgets/time_picker.dart';
 import 'controllers/time_picker_controller.dart';
-
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'dart:typed_data';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +15,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => MainScreen(),
         '/alarm': (context) => AlarmSettingScreen(),
+        '/wake_up': (context) => WakeUpScreen(),
+        '/qr_scan': (context) => QRScanScreen(),
       },
       initialRoute: '/',
     );
@@ -35,7 +38,7 @@ class MainScreen extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pushNamed(context, '/alarm');
+            Navigator.pushNamed(context, '/wake_up');
           },
           child: Text(
             'Get Start!',
@@ -87,6 +90,76 @@ class AlarmSettingScreen extends StatelessWidget {
 
   void resetTime() {
     _controller.setTime(DateTime.now());
+  }
+}
+
+class WakeUpScreen extends StatelessWidget {
+  final _controller = TimePickerController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1B1826),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton(
+              child: Text(
+                'Scan QR Code',
+                style: TextStyle(fontSize: 16),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/qr_scan');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFFD4E9E4),
+                onPrimary: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class QRScanScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mobile Scanner')),
+      body: MobileScanner(
+        fit: BoxFit.contain,
+        controller: MobileScannerController(
+          // facing: CameraFacing.back,
+          // torchEnabled: false,
+          returnImage: true,
+        ),
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
+          }
+          if (image != null) {
+            showDialog(
+              context: context,
+              builder: (context) =>
+                  Image(image: MemoryImage(image)),
+            );
+            Future.delayed(const Duration(seconds: 5), () {
+              Navigator.pop(context);
+            });
+          }
+        },
+      ),
+    );
   }
 }
 
